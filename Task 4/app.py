5f9c52210343dccd62456ae5e78ac6a2349e819d
Shizanourin <136820271+Shizanourin@users.noreply.github.com>
2025-09-26 18:31:18 +0530

    user greeting

--- Task 4/app.py
+++ Task 4/app.py
@@ -1,6 +1,9 @@
 # app.py (updated version)
 def main():
     print("Welcome to the DevOps Project!")
-   
+    greet_user("Developer")
+    
+def greet_user(name):
+    print(f"Hello, {name}!")
 if __name__ == "__main__":
     main()
